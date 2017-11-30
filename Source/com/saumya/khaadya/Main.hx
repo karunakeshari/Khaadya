1948bd37cad7850a81ec9585e6a5cc031407ac97
saumya <2saumyaray@gmail.com>
2017-11-30 19:57:04 +0530

    Game Entry

--- Source/com/saumya/khaadya/Main.hx
+++ Source/com/saumya/khaadya/Main.hx
@@ -1,7 +1,13 @@
 package com.saumya.khaadya;
 
-
+import openfl.display.Bitmap;
+import openfl.display.BitmapData;
 import openfl.display.Sprite;
+import openfl.events.Event;
+import openfl.events.KeyboardEvent;
+import openfl.system.Capabilities;
+import openfl.Assets;
+import openfl.Lib;
 
 
 class Main extends Sprite {
@@ -11,8 +17,20 @@ class Main extends Sprite {
 		
 		super ();
 		
+		initialize ();
+		construct ();
 		
+		resize (stage.stageWidth, stage.stageHeight);
+		stage.addEventListener (Event.RESIZE, stage_onResize);
 		
+	}
+
+	private function initialize():Void{}
+	private function construct():Void{}
+
+	private function resize(newWidth:Int, newHeight:Int):Void{}
+	private function stage_onResize(event:Event):Void{
+		resize (stage.stageWidth, stage.stageHeight);
 	}
 	
 	
