5ee76d5872cb98975ceac893185872b590a2220c
saumya <2saumyaray@gmail.com>
2017-12-12 21:25:49 +0530

    background added to the popup

--- Source/com/saumya/khaadya/ui/PopupYay.hx
+++ Source/com/saumya/khaadya/ui/PopupYay.hx
@@ -2,6 +2,7 @@
 package com.saumya.khaadya.ui;
 
 import openfl.display.Sprite;
+import openfl.display.Graphics;
 
 
 class PopupYay extends Sprite {
@@ -14,6 +15,12 @@ class PopupYay extends Sprite {
 		construct();
 	}
 	private function construct():Void{
+		// transparent BG
+		var bg: Graphics = this.graphics;
+		bg.beginFill(0xFF0000,0.6);
+		bg.drawRect(-200,-200,400,400);
+		bg.endFill();
+		//
 		var yayClip:Sprite = new ImgYay();// from SWF library
 		this.addChild(yayClip);
 	}
